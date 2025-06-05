356b778ee18be3ba163daa7513b79984e94db96c
NataFelipeS <nfelipe05@gmail.com>
2025-06-05 17:53:23 -0300

    Evoluindo bastante

--- Subindo_o_nivel/ex1.sql
+++ Subindo_o_nivel/ex1.sql
@@ -1,7 +1,7 @@
 -- Liste os nomes dos 100 primeiros produtos, com o nome da subcategoria e da categoria correspondente.
 
 SELECT 
-TOP 100 * 
+    TOP 100 * 
 FROM PRODUCTION.PRODUCT PD
-INNER JOIN Production.ProductSubcategory PS ON PS.ProductSubcategoryID = PD.ProductSubcategoryID
-INNER JOIN Production.ProductCategory PC ON PC.ProductCategoryID = PS.ProductCategoryID
+    INNER JOIN Production.ProductSubcategory PS ON PS.ProductSubcategoryID = PD.ProductSubcategoryID
+    INNER JOIN Production.ProductCategory PC ON PC.ProductCategoryID = PS.ProductCategoryID
